7c672111114cd0e015fad85bfef2c37b9f0c1890
Rodrigo Rodrigues <prorodrigorodrigues@gmail.com>
2023-08-17 15:59:38 -0300

    ativando proteção de rotas por meio de autenticação

--- start/routes/posts.ts
+++ start/routes/posts.ts
@@ -1,3 +1,7 @@
 import Route from '@ioc:Adonis/Core/Route'
 
-Route.resource('/posts', 'PostsController').apiOnly()
+Route.resource('/posts', 'PostsController').apiOnly().middleware({
+  store: ['auth'],
+  update: ['auth'],
+  destroy: ['auth'],
+})
